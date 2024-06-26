c369213aa0d3aba2394e46cddd13cfc44d4fc141
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-06-26 20:51:08 +0200

    Изменено условие для DEBUG в головном urls

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,8 +8,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
     path('', include('blog.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
