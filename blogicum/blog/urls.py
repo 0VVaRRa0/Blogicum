835844a1eee7e754f0824146af3421e1f6843696
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-06-27 16:01:48 +0200

    Добавлено CBV ProfileListView для просмотра страницы пользователя

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,12 +5,16 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.HomepageView.as_view(), name='index'),
+    path('', views.HomepageListView.as_view(), name='index'),
     path(
         'posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'
     ),
     path(
         'category/<slug:category_slug>/',
-        views.CategoryPostsView.as_view(), name='category_posts'
+        views.CategoryPostsListView.as_view(), name='category_posts'
     ),
+    path(
+        'profile/<slug:username>',
+        views.ProfileListView.as_view(), name='profile'
+    )
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.views.generic import DetailView, ListView
@@ -7,7 +8,7 @@ from .models import Category, Post
 POSTS = Post.objects.select_related('category', 'location', 'author')
 
 
-class HomepageView(ListView):
+class HomepageListView(ListView):
     model = Post
     template_name = "blog/index.html"
     paginate_by = 10
@@ -33,7 +34,7 @@ class PostDetailView(DetailView):
         return context
 
 
-class CategoryPostsView(ListView):
+class CategoryPostsListView(ListView):
     model = Post
     template_name = 'blog/category.html'
     paginate_by = 10
@@ -51,6 +52,27 @@ class CategoryPostsView(ListView):
         qs = super().get_queryset()
         return qs.filter(
             category__slug=self.kwargs['category_slug'],
+            is_published=True,
+            pub_date__lte=timezone.now(),
+        )
+
+
+class ProfileListView(ListView):
+    model = Post
+    template_name = 'blog/profile.html'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = get_object_or_404(
+            User.objects.filter(username=self.kwargs['username'])
+        )
+        return context
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            author__username=self.kwargs.get('username'),
+            category__is_published=True,
             is_published=True,
             pub_date__lte=timezone.now(),
         )
