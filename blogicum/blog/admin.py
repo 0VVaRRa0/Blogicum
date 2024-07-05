da00f932423d2120ebee9a4060691d93f24b3f36
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-07-05 21:20:28 +0200

    Настроена админ-зона

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -3,17 +3,26 @@ from django.contrib import admin
 from .models import Category, Comment, Location, Post
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_published', 'category')
-    list_editable = ('is_published', 'category')
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_published')
+    list_display = ('title', 'is_published', 'slug')
     list_editable = ('is_published',)
 
 
-admin.site.register(Location)
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'created_at', 'post', 'text')
+    search_fields = ('post',)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('category',)
+    list_display = ('title', 'is_published', 'category')
+    list_editable = ('is_published', 'category')
+    search_fields = ('title',)
