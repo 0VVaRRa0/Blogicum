37c681bef6a90f1e0fa9b3329aabdad68c81c59a
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-07-06 07:45:33 +0200

    В CommentForm добавлен виджет TextArea для управления размером окна

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -4,6 +4,9 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 3}), label='Текст комментария'
+    )
 
     class Meta:
         model = Comment
